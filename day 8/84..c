3e36acc181fa61d61d00aa32dfe87b1a6325363a
ashwini.o <aswiniaishu213@gmail.com>
2025-11-27 07:20:32 +0530

    day 9

--- day 8/84..c
+++ day 8/84..c
@@ -1,9 +1,17 @@
 #include<stdio.h>
+struct student{
+    char name [30];
+    int roll_num;    
+};
+
 int main(){
-    int a=200;
-    int *ptr=&a;
-    printf("value of a:%d\n",a);
-    printf("value of a using *ptr:%d \n",*ptr);
-    printf("value of ptr: %p\n",ptr);
-    printf("value of ptr using a:%p",&a);
+    struct student st1;
+    printf("enter a name :\n");
+    scanf("%s",st1.name);
+    printf("enter a roll number:\n");
+    scanf("%d",&st1.roll_num);
+
+    printf("student details: \n");
+    printf("name: %s\n",st1.name);
+    printf("roll no: %d\n",st1.roll_num);
 }
